a2add873c23bb9f5a749b5e73fa099aff981b6c1
Timofey Kuznetsov <tima1702@gmail.com>
2018-12-10 16:06:04 +0300

    Test CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Awesome Update!</h1>
+          <h1>Awesome Update! # 1</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
