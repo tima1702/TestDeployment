543b6f7b5b2f6a45bb1bcc4192b79ebc363262ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-10 13:27:03 +0000

    Deploy tima1702/TestDeployment to github.com/tima1702/TestDeployment.git:gh-pages

---
+++ precache-manifest.cd4d23815bc6cffbea371447c3c5bcd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f48d0299fe49c3d8d52",
+    "url": "/TestDeployment/static/css/main.ec86963b.chunk.css"
+  },
+  {
+    "revision": "0f48d0299fe49c3d8d52",
+    "url": "/TestDeployment/static/js/main.0f48d029.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/TestDeployment/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "e84e5505ab8407d08334",
+    "url": "/TestDeployment/static/js/runtime~main.e84e5505.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestDeployment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fb508fe6a3b0433fb15ceeddeab6272",
+    "url": "/TestDeployment/index.html"
+  }
+];
